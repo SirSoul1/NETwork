f189f83280481291ea7eb39254dc0345c97be3fb
Selcuk Gorsay <sgorsay30@gmail.com>
2024-08-14 11:24:36 +0300

    implemented post create, update and, delete functionality for users to communicate within site

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from .views import home, post_detail
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', home, name='blog-home'),
-    path('post/<int:pk>/', post_detail, name='post-detail'),
-    path('home/', views.home, name='blog-home'),
+    path('home/', PostListView.as_view(), name='blog-home'),
+    path('home/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('home/post/new/', PostCreateView.as_view(), name='post-create'),
+    path('home/post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('home/post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('contact/', views.contact, name='blog-contact'),
     path('search/', views.search, name='blog-search'),
@@ -14,3 +17,5 @@ urlpatterns = [
     path('register/', views.register, name='blog-register'),
     
 ]
+
+
