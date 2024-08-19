17b73721fa2bb8ae75243a53ff8fb901d2ef7b49
Selcuk Gorsay <sgorsay30@gmail.com>
2024-08-19 13:38:01 +0300

    simple design changes and need to work on a fix for React integration

--- static/js/App.js
+++ static/js/App.js
@@ -1,10 +1,11 @@
 // App.js
 import React from 'react';
+import './styles.css';  // Ensure the correct path is used here
 
 function App() {
     return (
         <div>
-            <h1>Hello, React!</h1>
+            <h1 className="header">Hello, React!</h1>
         </div>
     );
 }
