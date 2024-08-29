e4f524e8c4597a8430fd79c7b41da167c1df08df
Selcuk Gorsay <sgorsay30@gmail.com>
2024-08-29 10:42:15 +0300

    im losing my sanity

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,9 +139,9 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_DEFAULT_ACL = 'public-read'
+
 AWS_S3_REGION_NAME='us-east-1'
-AWS_DEFAULT_ACL = 'public-read'
+
 
 
 STATIC_URL = 'static/'
@@ -150,7 +150,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/profile_pics/'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
