51e3e8bcd1990976f5c08832ab4df25224644ee3
Selcuk Gorsay <sgorsay30@gmail.com>
2024-08-29 09:56:44 +0300

    trying to get AWS to workkkkkkk

--- mysite/settings.py
+++ mysite/settings.py
@@ -141,7 +141,7 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_REGION_NAME='us-east-1'
-
+AWS_DEFAULT_ACL = 'public-read'
 
 
 STATIC_URL = 'static/'
