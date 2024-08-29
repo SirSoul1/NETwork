f1e1da3def36ca5743902ef6803c07de19dee3a3
Selcuk Gorsay <sgorsay30@gmail.com>
2024-08-29 09:32:17 +0300

    trying to get AWS to workkkk

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,6 +139,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = 'public-read'
+
 
 
 STATIC_URL = 'static/'
